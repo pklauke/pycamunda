55c205998f7451ad5f02831c0094b39b6c1e4824
Peter Klauke <peter.klauke@tu-dortmund.de>
2020-03-10 21:00:32 +0100

    Add setup script.

---
+++ setup.py
@@ -0,0 +1,12 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='pycamunda',
+      version='0.1',
+      description='A high-level framework for communicating with the workflow and decision automation engine Camunda.',
+      url='https://github.com/pklauke/pycamunda',
+      author='Peter Klauke',
+      packages=['pycamunda'],
+      zip_safe=False)
