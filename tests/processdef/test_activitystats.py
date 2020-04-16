e3cd847b95f73f6cce3eef727158805e2dc57f8f
Peter Klauke <peter.klauke@tu-dortmund.de>
2020-04-16 20:25:05 +0200

    Fix incorrect variable name.

--- tests/processdef/test_activitystats.py
+++ tests/processdef/test_activitystats.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
+
 import pytest
 
 import pycamunda.processdef
 
 
 def test_activitystats_load(my_activity_stats_json):
-    activity_instance = pycamunda.processdef.ActivityStats.load(my_activity_stats_json)
+    activity_stats = pycamunda.processdef.ActivityStats.load(my_activity_stats_json)
 
-    assert activity_instance.id_ == my_activity_stats_json['id']
-    assert activity_instance.instances == my_activity_stats_json['instances']
-    assert activity_instance.failed_jobs == my_activity_stats_json['failedJobs']
+    assert activity_stats.id_ == my_activity_stats_json['id']
+    assert activity_stats.instances == my_activity_stats_json['instances']
+    assert activity_stats.failed_jobs == my_activity_stats_json['failedJobs']
 
 
 def test_activitystats_load_raises_keyerror(my_activity_stats_json):
