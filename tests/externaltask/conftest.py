9993c722d554188a45dfd16fc0b4f7aa067df9b6
Peter Klauke <peter.klauke@tu-dortmund.de>
2020-05-04 22:02:00 +0200

    Add unit tests for external task get list.

--- tests/externaltask/conftest.py
+++ tests/externaltask/conftest.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import datetime as dt
+
 import pytest
+
+import pycamunda.base
 
 
 @pytest.fixture
@@ -24,4 +28,69 @@ def my_externaltask_json():
         'suspended': True,
         'businessKey': 'aBusinessKey',
         'variables': {'aVar': {'value': 'aVal', 'type': 'String', 'valueInfo': {}}}
+    }
+
+
+@pytest.fixture
+def getlist_input():
+    return {
+        'id_': 'anId',
+        'topic_name': 'aTopicName',
+        'worker_id': 'aWorkerId',
+        'locked': True,
+        'not_locked': True,
+        'with_retries_left': True,
+        'no_retries_left': True,
+        'lock_expiration_after': dt.datetime(year=2020, month=1, day=1, hour=1, minute=1, second=1),
+        'lock_expiration_before': dt.datetime(
+            year=2020, month=1, day=1, hour=1, minute=1, second=1
+        ),
+        'activity_id': 'anActivityId',
+        'activity_id_in': [],
+        'execution_id': 'anExecutionId',
+        'process_instance_id': 'aProcessInstanceId',
+        'process_definition_id': 'aProcessDefinitionId',
+        'tenant_id_in': [],
+        'active': True,
+        'priority_higher_equals': 10,
+        'priority_lower_equals': 20,
+        'suspended': True,
+        'sort_by': 'id_',
+        'ascending': True,
+        'first_result': 1,
+        'max_results': 100,
+        'request_error_details': True
+    }
+
+
+@pytest.fixture
+def getlist_output():
+    return {
+        'externalTaskId': 'anId',
+        'topicName': 'aTopicName',
+        'workerId': 'aWorkerId',
+        'locked': True,
+        'notLocked': True,
+        'withRetriesLeft': True,
+        'noRetriesLeft': True,
+        'lockExpirationAfter': pycamunda.base.isoformat(
+            dt.datetime(year=2020, month=1, day=1, hour=1, minute=1, second=1)
+        ),
+        'lockExpirationBefore': pycamunda.base.isoformat(
+            dt.datetime(year=2020, month=1, day=1, hour=1, minute=1, second=1)
+        ),
+        'activityId': 'anActivityId',
+        'activityIdIn': [],
+        'executionId': 'anExecutionId',
+        'processInstanceId': 'aProcessInstanceId',
+        'processDefinitionId': 'aProcessDefinitionId',
+        'tenantIdIn': [],
+        'active': True,
+        'priorityHigherThanOrEquals': 10,
+        'priorityLowerThanOrEquals': 20,
+        'suspended': True,
+        'sortBy': 'id',
+        'sortOrder': 'asc',
+        'firstResult': 1,
+        'maxResults': 100
     }
---
+++ tests/externaltask/test_getlist.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+import unittest.mock
+
+import pytest
+
+import pycamunda.externaltask
+from tests.mock import raise_requests_exception_mock, not_ok_response_mock
+
+
+def test_getlist_params(engine_url, getlist_input, getlist_output):
+    get_tasks = pycamunda.externaltask.GetList(url=engine_url, **getlist_input)
+
+    assert get_tasks.url == engine_url + '/external-task'
+    assert get_tasks.query_parameters() == getlist_output
+    assert get_tasks.body_parameters() == {}
+
+
+@unittest.mock.patch('requests.get')
+@unittest.mock.patch('pycamunda.externaltask.ExternalTask', unittest.mock.MagicMock())
+def test_getlist_calls_requests(mock, engine_url):
+    get_tasks = pycamunda.externaltask.GetList(url=engine_url)
+    get_tasks()
+
+    assert mock.called
+
+
+@unittest.mock.patch('requests.get', raise_requests_exception_mock)
+def test_getlist_raises_pycamunda_exception(engine_url):
+    get_tasks = pycamunda.externaltask.GetList(url=engine_url)
+    with pytest.raises(pycamunda.PyCamundaException):
+        get_tasks()
+
+
+@unittest.mock.patch('requests.get', not_ok_response_mock)
+@unittest.mock.patch('pycamunda.externaltask.ExternalTask', unittest.mock.MagicMock())
+@unittest.mock.patch('pycamunda.base._raise_for_status')
+def test_getlist_raises_for_status(mock, engine_url):
+    get_tasks = pycamunda.externaltask.GetList(url=engine_url)
+    get_tasks()
+
+    assert mock.called
+
+
+@unittest.mock.patch('requests.get', unittest.mock.MagicMock())
+@unittest.mock.patch('pycamunda.externaltask.ExternalTask', unittest.mock.MagicMock())
+def test_getlist_returns_tuple(engine_url):
+    get_tasks = pycamunda.externaltask.GetList(url=engine_url)
+    tasks = get_tasks()
+
+    assert isinstance(tasks, tuple)
