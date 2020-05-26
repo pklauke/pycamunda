ed0d00afee5a9a3732aa8afe260509b23bc85989
Peter Klauke <peter.klauke@tu-dortmund.de>
2020-05-26 17:52:07 +0200

    Add unit tests for task unclaim.

---
+++ tests/task/test_unclaim.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+import unittest.mock
+
+import pytest
+
+import pycamunda.task
+from tests.mock import raise_requests_exception_mock, not_ok_response_mock
+
+
+def test_unclaim_params(engine_url):
+    unclaim_task = pycamunda.task.Unclaim(url=engine_url, id_='anId')
+
+    assert unclaim_task.url == engine_url + '/task/anId/unclaim'
+    assert unclaim_task.query_parameters() == {}
+    assert unclaim_task.body_parameters() == {}
+
+
+@unittest.mock.patch('requests.post')
+def test_unclaim_calls_requests(mock, engine_url):
+    unclaim_task = pycamunda.task.Unclaim(url=engine_url, id_='anId')
+    unclaim_task()
+
+    assert mock.called
+
+
+@unittest.mock.patch('requests.post', raise_requests_exception_mock)
+def test_unclaim_raises_pycamunda_exception(engine_url):
+    unclaim_task = pycamunda.task.Unclaim(url=engine_url, id_='anId')
+    with pytest.raises(pycamunda.PyCamundaException):
+        unclaim_task()
+
+
+@unittest.mock.patch('requests.post', not_ok_response_mock)
+@unittest.mock.patch('pycamunda.base._raise_for_status')
+def test_unclaim_raises_for_status(mock, engine_url):
+    unclaim_task = pycamunda.task.Unclaim(url=engine_url, id_='anId')
+    unclaim_task()
+
+    assert mock.called
+
+
+@unittest.mock.patch('requests.post', unittest.mock.MagicMock())
+def test_unclaim_returns_none(engine_url):
+    unclaim_task = pycamunda.task.Unclaim(url=engine_url, id_='anId')
+    result = unclaim_task()
+
+    assert result is None
