1ac36f67ebc64a47a698da6f30786f5ca6e2b44b
Peter Klauke <peter.klauke@tu-dortmund.de>
2020-05-27 18:39:59 +0200

    Add unit tests for task complete.

---
+++ tests/task/test_complete.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+import unittest.mock
+
+import pytest
+
+import pycamunda.task
+import pycamunda.variable
+from tests.mock import raise_requests_exception_mock, not_ok_response_mock
+
+
+def mock_post_with_variables_in_return(*args, json=None):
+    class Response:
+        def json(self):
+            return json['variables']
+
+    return Response()
+
+
+def test_complete_params(engine_url):
+    complete_task = pycamunda.task.Complete(
+        url=engine_url, id_='anId', with_variables_in_return=True
+    )
+    complete_task.add_variable(name='aVar', value='aVal', type_='String', value_info='')
+
+    assert complete_task.url == engine_url + '/task/anId/complete'
+    assert complete_task.query_parameters() == {}
+    assert complete_task.body_parameters() == {
+        'variables': {'aVar': {'value': 'aVal', 'type': 'String', 'valueInfo': ''}},
+        'withVariablesInReturn': True
+    }
+
+
+@unittest.mock.patch('requests.post')
+def test_complete_calls_requests(mock, engine_url):
+    complete_task = pycamunda.task.Complete(url=engine_url, id_='anId')
+    complete_task()
+
+    assert mock.called
+
+
+@unittest.mock.patch('requests.post', raise_requests_exception_mock)
+def test_complete_raises_pycamunda_exception(engine_url):
+    complete_task = pycamunda.task.Complete(url=engine_url, id_='anId')
+    with pytest.raises(pycamunda.PyCamundaException):
+        complete_task()
+
+
+@unittest.mock.patch('requests.post', not_ok_response_mock)
+@unittest.mock.patch('pycamunda.base._raise_for_status')
+def test_complete_raises_for_status(mock, engine_url):
+    complete_task = pycamunda.task.Complete(url=engine_url, id_='anId')
+    complete_task()
+
+    assert mock.called
+
+
+@unittest.mock.patch('requests.post', unittest.mock.MagicMock())
+def test_complete_returns_none(engine_url):
+    complete_task = pycamunda.task.Complete(url=engine_url, id_='anId')
+    result = complete_task()
+
+    assert result is None
+
+
+@unittest.mock.patch('requests.post', mock_post_with_variables_in_return)
+def test_complete_returns_variables(engine_url):
+    complete_task = pycamunda.task.Complete(
+        url=engine_url, id_='anId', with_variables_in_return=True
+    )
+    complete_task.add_variable(name='aVar', value='aVal')
+    variables = complete_task()
+
+    assert isinstance(variables, dict)
+    assert 'aVar' in variables
+    assert all(isinstance(variable, pycamunda.variable.Variable) for variable in variables.values())
+    assert variables['aVar'].value == 'aVal'
