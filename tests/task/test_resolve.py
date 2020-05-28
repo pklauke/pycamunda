88d2e7abc9adb43171a5dd273f7f5009285fb7a1
Peter Klauke <peter.klauke@tu-dortmund.de>
2020-05-28 18:27:47 +0200

    Add unit tests for task resolve.

---
+++ tests/task/test_resolve.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+import unittest.mock
+
+import pytest
+
+import pycamunda.task
+import pycamunda.variable
+from tests.mock import raise_requests_exception_mock, not_ok_response_mock
+
+
+def test_resolve_params(engine_url):
+    resolve_task = pycamunda.task.Resolve(url=engine_url, id_='anId')
+    resolve_task.add_variable(name='aVar', value='aVal', type_='String', value_info='')
+
+    assert resolve_task.url == engine_url + '/task/anId/resolve'
+    assert resolve_task.query_parameters() == {}
+    assert resolve_task.body_parameters() == {
+        'variables': {'aVar': {'value': 'aVal', 'type': 'String', 'valueInfo': ''}}
+    }
+
+
+@unittest.mock.patch('requests.post')
+def test_resolve_calls_requests(mock, engine_url):
+    resolve_task = pycamunda.task.Resolve(url=engine_url, id_='anId')
+    resolve_task()
+
+    assert mock.called
+
+
+@unittest.mock.patch('requests.post', raise_requests_exception_mock)
+def test_resolve_raises_pycamunda_exception(engine_url):
+    resolve_task = pycamunda.task.Resolve(url=engine_url, id_='anId')
+    with pytest.raises(pycamunda.PyCamundaException):
+        resolve_task()
+
+
+@unittest.mock.patch('requests.post', not_ok_response_mock)
+@unittest.mock.patch('pycamunda.base._raise_for_status')
+def test_resolve_raises_for_status(mock, engine_url):
+    resolve_task = pycamunda.task.Resolve(url=engine_url, id_='anId')
+    resolve_task()
+
+    assert mock.called
+
+
+@unittest.mock.patch('requests.post', unittest.mock.MagicMock())
+def test_resolve_returns_none(engine_url):
+    resolve_task = pycamunda.task.Resolve(url=engine_url, id_='anId')
+    result = resolve_task()
+
+    assert result is None
