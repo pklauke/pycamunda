6bdfdd2cbe8d2c6cd06618c6984d58f181baf4f7
Peter Klauke <peter.klauke@tu-dortmund.de>
2020-06-02 19:09:08 +0200

    Add unit tests for filter get list.

--- tests/filter/conftest.py
+++ tests/filter/conftest.py
@@ -14,3 +14,34 @@ def my_filter_json():
         'properties': {'prop_var': 'prop_val'},
         'itemCount': 1
     }
+
+
+@pytest.fixture
+def getlist_input():
+    return {
+        'id_': 'anId',
+        'name': 'aName',
+        'name_like': 'aNam',
+        'owner': 'anOwner',
+        'item_count': True,
+        'sort_by': 'id_',
+        'ascending': False,
+        'first_result': 1,
+        'max_results': 10
+    }
+
+
+@pytest.fixture
+def getlist_output():
+    return {
+        'filterId': 'anId',
+        'name': 'aName',
+        'nameLike': 'aNam',
+        'owner': 'anOwner',
+        'itemCount': True,
+        'sortBy': 'filterId',
+        'sortOrder': 'desc',
+        'firstResult': 1,
+        'maxResults': 10,
+        'resourceType': 'Task'
+    }
---
+++ tests/filter/test_getlist.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+import unittest.mock
+
+import pytest
+
+import pycamunda.filter
+from tests.mock import raise_requests_exception_mock, not_ok_response_mock
+
+
+def test_getlist_params(engine_url, getlist_input, getlist_output):
+    get_incidents = pycamunda.filter.GetList(url=engine_url, **getlist_input)
+
+    assert get_incidents.url == engine_url + '/filter'
+    assert get_incidents.query_parameters() == getlist_output
+    assert get_incidents.body_parameters() == {}
+
+
+@unittest.mock.patch('requests.get')
+def test_getlist_calls_requests(mock, engine_url):
+    get_incidents = pycamunda.filter.GetList(url=engine_url)
+    get_incidents()
+
+    assert mock.called
+
+
+@unittest.mock.patch('requests.get', raise_requests_exception_mock)
+def test_getlist_raises_pycamunda_exception(engine_url):
+    get_incidents = pycamunda.filter.GetList(url=engine_url)
+    with pytest.raises(pycamunda.PyCamundaException):
+        get_incidents()
+
+
+@unittest.mock.patch('requests.get', not_ok_response_mock)
+@unittest.mock.patch('pycamunda.filter.Filter', unittest.mock.MagicMock())
+@unittest.mock.patch('pycamunda.base._raise_for_status')
+def test_getlist_raises_for_status(mock, engine_url):
+    get_incidents = pycamunda.filter.GetList(url=engine_url)
+    get_incidents()
+
+    assert mock.called
+
+
+@unittest.mock.patch('requests.get', unittest.mock.MagicMock())
+@unittest.mock.patch('pycamunda.filter.Filter', unittest.mock.MagicMock())
+def test_getlist_returns_incidents_tuple(engine_url):
+    get_incidents = pycamunda.filter.GetList(url=engine_url)
+    filters = get_incidents()
+
+    assert isinstance(filters, tuple)
+    assert all(isinstance(filter_, pycamunda.filter.Filter) for filter_ in filters)
