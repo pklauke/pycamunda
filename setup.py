7c4cd8015340760ad7ee6024cfd16279dc9774fe
Peter Klauke <peter.klauke@tu-dortmund.de>
2020-06-06 15:46:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
+import setuptools
 
-setup(name='pycamunda',
-      version='0.1',
-      description='A high-level framework for communicating with the workflow and decision automation engine Camunda.',
-      url='https://github.com/pklauke/pycamunda',
-      author='Peter Klauke',
-      packages=['pycamunda'],
-      zip_safe=False)
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pycamunda',
+    version='0.0.1',
+    author='Peter Klauke',
+    description='A high-level framework for communicating with the workflow and decision automation engine Camunda.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/pklauke/pycamunda',
+    packages=['pycamunda'],
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=['requests>=2.0.0']
+)
